1c6a69276db8f9ecedab8314b0df628a0054e8f3
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-10 10:49:57 -0700

    format json api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   
   # DEVISE API ROUTES
-  devise_for :users,
-             path: '',
-             path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
-             },
-             controllers: {
-               sessions: 'sessions',
-               registrations: 'registrations'
-             }
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users,
+                path: '',
+                path_names: {
+                  sign_in: 'login',
+                  sign_out: 'logout',
+                  registration: 'signup'
+                },
+                controllers: {
+                  sessions: 'sessions',
+                  registrations: 'registrations'
+                }
+    end
+  end
+
 end

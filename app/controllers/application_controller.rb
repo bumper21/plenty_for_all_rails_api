948b20988bd13838101ef17f686517533ed8fdb5
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-10 11:30:01 -0700

    syntax err

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanatizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
 end

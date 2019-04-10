878009da3a94e06afce0dc0bdd3dcdc8902901ce
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-10 10:28:42 -0700

    initial custom routes, need change for api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  respond_to :json
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanatizer.permit(:sign_up, keys: [:username])
   end
-  
+
 end

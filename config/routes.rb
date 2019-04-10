43d5a92fb428116e0c5b0af80a79e1c06279d9fa
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-10 10:12:51 -0700

    adding username parameters to on sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # DEVISE API ROUTES
+  devise_for :users,
+             path: '',
+             path_names: {
+               
+             }
 end

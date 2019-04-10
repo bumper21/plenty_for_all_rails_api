8cf4f427942d2fb990b6ae047b436e8dbbd99b39
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-10 11:04:36 -0700

    regis contr #create

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
 
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    render_resource(resource)
+  end
+
 end

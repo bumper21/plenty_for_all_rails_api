d9b7a4395cc05ced3edd7fe37e62dbfbab812cb6
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-11 16:45:28 -0700

    minor change

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   def create
     user = User.find_by_email(sign_in_params[:email])
-    puts("RESOURCE NAME", resource_name)
     if user && user.valid_password?(sign_in_params[:password])
       @current_user = user
     else

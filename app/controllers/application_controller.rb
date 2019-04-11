a5c3665652acca5910c4692c1881cacaaa75480c
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-10 17:40:11 -0700

    Still not getting user params for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
+
   respond_to :json
 
   def render_resource(resource)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,10 +7,11 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
     jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
+      ['POST', %r{^/api/v1/users/login$}],
+      ['POST', %r{^/api/v1/users/signup$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$ }]
+      ['DELETE', %r{^/api/v1/users/logout$ }]
     ]
     jwt.expiration_time = 1.day.to_i
   end

ee4217f411704ef99d1177649ae2b69a10303c8b
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-04-16 11:13:02 -0700

    sess controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,26 @@
 class Api::V1::SessionsController < Devise::SessionsController
-  before_action :authenticate_user!
+  skip_before_action :verify_signed_out_user
 
   def create
-    user = User.find_by_email(sign_in_params[:email])
-    if user && user.valid_password?(sign_in_params[:password])
-      current_user = user
-      # render json: current_user
-      puts("RESOURCE", resource)
-      sign_in(resource_name, resource)
+    super
+    # THE CODE BELOW WILL MOST LIKELY NEED TO BE USED LATER ON
+    # WHEN WE ARE TESTING MAILERS ETC ON FRONT END
+
+    # user = User.find_by_email(sign_in_params[:email])
+    # if user && user.valid_password?(sign_in_params[:password])
+    #   current_user = user
+    #   # render json: current_user
       
-    else
-      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
-    end
+      
+    # else
+    #   # render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+    # end
+
+  end
+
+  def destroy
+    super
+    
   end
 
   private
